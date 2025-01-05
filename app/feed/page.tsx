eb3948acb736025b08617e24bcbb49d926f02d2e
Shreekant Kalwar <shreekantkalwar@gmail.com>
2025-01-05 16:49:47 +0530

    Feed Skeleton

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -10,6 +10,7 @@ import {
     Link,
     Image,
     User,
+    Skeleton,
 } from "@nextui-org/react";
 import { HeartFilledIcon } from "@/components/icons";
 import { HeartIcon } from "@/components/HeartIcon";
@@ -92,7 +93,31 @@ const Feed = () => {
                     </Card>
                 ))
             ) : (
-                <p>No posts available.</p>
+                <Card className="w-[500px]  space-y-5 p-4" radius="lg">
+                    <div className="max-w-[300px] w-full flex items-center gap-3">
+                        <div>
+                            <Skeleton className="flex rounded-full w-12 h-12" />
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <Skeleton className="h-3 w-3/5 rounded-lg" />
+                            <Skeleton className="h-3 w-4/5 rounded-lg" />
+                        </div>
+                    </div>
+                    <Skeleton className="rounded-lg">
+                        <div className="h-[300px] rounded-lg bg-default-300" />
+                    </Skeleton>
+                    <div className="space-y-3 ">
+                        <Skeleton className="w-3/5 rounded-lg">
+                            <div className="h-3 w-3/5 rounded-lg bg-default-200" />
+                        </Skeleton>
+                        <Skeleton className="w-4/5 rounded-lg">
+                            <div className="h-3 w-4/5 rounded-lg bg-default-200" />
+                        </Skeleton>
+                        <Skeleton className="w-2/5 rounded-lg">
+                            <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+                        </Skeleton>
+                    </div>
+                </Card>
             )}
         </div>
     );
