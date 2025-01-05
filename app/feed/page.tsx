6ea9250d2c4fe878a52c86136cedca0a354315ae
Shreekant Kalwar <shreekantkalwar@gmail.com>
2025-01-06 00:41:22 +0530

    Feed Page Update

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -191,9 +191,29 @@ const Feed: React.FC = () => {
                 ))
             ) : (
                 <Card className="w-auto space-y-5 p-4" radius="lg">
-                    <Skeleton className="flex rounded-full w-12 h-12" />
-                    <Skeleton className="rounded-lg h-48" />
-                    <Skeleton className="h-3 w-4/5 rounded-lg" />
+                    <div className="w-80  flex items-center gap-3">
+                        <div>
+                            <Skeleton className="flex rounded-full w-12 h-12" />
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <Skeleton className="h-3 w-3/5 rounded-lg" />
+                            <Skeleton className="h-3 w-4/5 rounded-lg" />
+                        </div>
+                    </div>
+                    <Skeleton className="rounded-lg">
+                        <div className="h-48 rounded-lg bg-default-300" />
+                    </Skeleton>
+                    <div className="space-y-3 ">
+                        <Skeleton className="w-3/5 rounded-lg">
+                            <div className="h-3 w-3/5 rounded-lg bg-default-200" />
+                        </Skeleton>
+                        <Skeleton className="w-4/5 rounded-lg">
+                            <div className="h-3 w-4/5 rounded-lg bg-default-200" />
+                        </Skeleton>
+                        <Skeleton className="w-2/5 rounded-lg">
+                            <div className="h-3 w-2/5 rounded-lg bg-default-300" />
+                        </Skeleton>
+                    </div>
                 </Card>
             )}
         </div>
